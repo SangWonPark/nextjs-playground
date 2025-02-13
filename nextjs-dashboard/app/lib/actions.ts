122a29a51969b7040053b1b31b93bda4a0b2b4f4
SangWonPark <steam93@naver.com>
2025-02-13 11:33:45 +0900

    [style/eslint] Apply Airbnb style guide rules

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -66,6 +66,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         `;
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.log('Database error:', error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
@@ -105,6 +106,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         `;
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.log('Database error:', error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
